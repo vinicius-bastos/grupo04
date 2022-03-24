4698eb4fdf98d25ef3a6989110ec6a2441b56ca1
thiago-yuiti <thiago.tomaru@bandtec.com.br>
2022-03-24 20:53:18 -0300

    Update with comments

--- python/v1.1/main.py
+++ python/v1.1/main.py
@@ -5,7 +5,7 @@ import config as sql
 
 def main():
     cnx,cursor = sql.conn()
-    transactions(cursor,1,600_000,100)
+    transactions(cursor,1,1_000_000,100)
     sql.desconect(cnx,cursor)
     
 def transactions(cursor,init,final,step):
@@ -13,12 +13,12 @@ def transactions(cursor,init,final,step):
     lista = []
     lista_tempo = []
     lista_memoria = []
-
+    # Criação dos valores do Range
     for n in range(init,final,step):
         lista.append(n)
     tracemalloc.start()
     tempo_inicial = (time.time())
-
+    # Inserção dos valores, pegando o tempo e o espaço ocupado na memória.
     while(lista != []):
         transaction.append(lista.pop())
         lista_memoria.append(tracemalloc.get_traced_memory()[0])
@@ -26,6 +26,7 @@ def transactions(cursor,init,final,step):
         lista_tempo.append(tempo_final)
     tracemalloc.stop()
     orderedTranscation = [num for num in reversed(transaction)]
+    # Inserção no Banco
     while(orderedTranscation!= []):
         sql.insert(cursor, 
                    orderedTranscation.pop(),
