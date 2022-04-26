0c23786b52049c2266fec3ecca3cc23a4b0ef2e7
Guilherme Paternezi <61800458+guilherme-paternezi@users.noreply.github.com>
2022-04-26 19:40:33 -0300

    Update CreateDatabase.sql

--- db/CreateDatabase.sql
+++ db/CreateDatabase.sql
@@ -1,24 +1,28 @@
-CREATE TABLE dados (
-	idDados int primary key auto_increment,
-    entrada int,
-    tempo float,
-    memoria float,
-    etnia varchar(20),
-    idade varchar(20),
-    sexo char(1),
-    grupo_risco boolean,
-    febre boolean,
-    coriza boolean,
-    dor_muscular boolean,
-    tosse boolean,
-    variante varchar(20),
-    classe_social varchar (20),
-    fk_estado char(2),
-    FOREIGN KEY (fk_estado) references estado(uf),
-    data_insercao date
+CREATE TABLE estado (
+	uf char(2) primary key
 );
 
-create table infos (
+CREATE TABLE dados (
+	idDados int primary key auto_increment,
+    	entrada int,
+    	tempo float,
+    	memoria float,
+    	etnia varchar(20),
+    	idade varchar(20),
+    	sexo char(1),
+    	grupo_risco boolean,
+    	febre boolean,
+    	coriza boolean,
+    	dor_muscular boolean,
+    	tosse boolean,
+    	variante varchar(20),
+    	classe_social varchar (20),
+    	fk_estado char(2),
+    	FOREIGN KEY (fk_estado) references estado(uf),
+    	data_insercao date
+);
+
+CREATE TABLE infos (
 	idInfo int primary key auto_increment,
 	fk_estado char(2),
 	FOREIGN KEY (fk_estado) references estado(uf),
@@ -28,39 +32,34 @@ create table infos (
 	maior_memoria int
 );
 
-create table estado (
-    uf char(2) primary key
-);
-
-INSERT INTO estado (uf)
-VALUES
-       	('AC'),
-	   	('AL'),
-		('AP'),
-	    ('AM'),
-	    ('BA'),
-	    ('CE'),
-	    ('DF'),
-	    ('ES'),
-	    ('GO'),
-	    ('MA'),
-	    ('MT'),
-	    ('MS'),
-        ('MG'),
-		('PA'),
-		('PB'),
-		('PR'),
-		('PE'),
-		('PI'),
-		('RJ'),
-		('RN'),
-		('RS'),
-		('RO'),
-		('RR'),
-		('SC'),
-		('SE'),
-		('SP'),
-		('TO');
+INSERT INTO estado (uf) VALUES
+('AC'),
+('AL'),
+('AP'),
+('AM'),
+('BA'),
+('CE'),
+('DF'),
+('ES'),
+('GO'),
+('MA'),
+('MT'),
+('MS'),
+('MG'),
+('PA'),
+('PB'),
+('PR'),
+('PE'),
+('PI'),
+('RJ'),
+('RN'),
+('RS'),
+('RO'),
+('RR'),
+('SC'),
+('SE'),
+('SP'),
+('TO');
 
 SET character_set_client = utf8;
 SET character_set_connection = utf8;
